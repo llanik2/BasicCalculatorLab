760bb414f4d513c5e0c26ab239b30977819737ce
llanik2 <llanik2@huskers.unl.edu>
2020-01-30 15:54:46 -0600

    Update Program.cs

--- BasicCalculatorLab/BasicCalculator/Program.cs
+++ BasicCalculatorLab/BasicCalculator/Program.cs
@@ -6,19 +6,46 @@ namespace BasicCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("*** BASIC CALCULATOR ***");
+            Console.WriteLine("*** BASIC WEATHER CALCULATOR ***");
 
-            Console.WriteLine("Enter the first number");
+            //DECLARE VARAIBLES
+            string input;
+            double temp;
+            double relhumidity;
+            double dewpoint;
+            double windspeed;
+            double windchill;
 
-            // int.Parse will take a string data type and convert it to an int data type
-            int firstNumber = int.Parse(Console.ReadLine());
+            //ASK USER FOR THE TEMP
+            Console.WriteLine("Enter the tempeture.");
+            input = Console.ReadLine();
+            temp = int.Parse(input);
 
-            Console.WriteLine("Enter the second number");
-            int secondNumber = int.Parse(Console.ReadLine());
 
-            int sum = firstNumber + secondNumber;
+            //ASK FOR REL HUMIDITY
+            Console.WriteLine("Enter relitive humidity.");
+            input = Console.ReadLine();
+            relhumidity = int.Parse(input);
 
-            Console.WriteLine("The answer is " + sum);
+            //CALCUATE DEW POINT
+            dewpoint = temp - 9 * (100 - relhumidity) / 25;
+
+            //PRINT DEW POINT
+            Console.WriteLine("Dew point = " + dewpoint);
+
+            //ASK FOR WINDSPEED
+            Console.WriteLine("Enter the windspeed.");
+            input = Console.ReadLine();
+            windspeed = int.Parse(input);
+
+            //CALCULATE WIND CHILL
+            double windpow; 
+               windpow = Math.Pow(windspeed, .16);
+            windchill = 35.74 + (.6215 * temp) - (37.75 * windpow) + (.4275 * temp * windpow);
+           
+            //PRINT WIND CHILL
+            Console.WriteLine("Wind chill = " + windchill);
+
         }
     }
 }
